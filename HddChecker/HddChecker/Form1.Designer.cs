4c45bb61cf304d17feed825b7208ef66dcdd954a
s-aoym <aoyama.shigeru@sharp.co.jp>
2022-10-25 10:54:11 +0900

    init

---
+++ HddChecker/HddChecker/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace HddChecker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.終了EToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer1 = new System.Timers.Timer();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.timer2 = new System.Timers.Timer();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.timer1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.timer2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(12, 83);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.終了EToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(113, 26);
+            // 
+            // 終了EToolStripMenuItem
+            // 
+            this.終了EToolStripMenuItem.Name = "終了EToolStripMenuItem";
+            this.終了EToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.終了EToolStripMenuItem.Text = "終了(&E)";
+            this.終了EToolStripMenuItem.Click += new System.EventHandler(this.終了EToolStripMenuItem_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 60000D;
+            this.timer1.SynchronizingObject = this;
+            this.timer1.Elapsed += new System.Timers.ElapsedEventHandler(this.timer1_Elapsed);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Text = "HddChecker";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 1000D;
+            this.timer2.SynchronizingObject = this;
+            this.timer2.Elapsed += new System.Timers.ElapsedEventHandler(this.timer2_Elapsed);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(355, 155);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.ControlBox = false;
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.Transparent;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.timer1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.timer2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Timers.Timer timer1;
+        private System.Windows.Forms.ToolStripMenuItem 終了EToolStripMenuItem;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Timers.Timer timer2;
+    }
+}
+
---
+++ HddChecker/HddChecker/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+using System.Diagnostics;
+using System.ServiceProcess;
+using System.Management;
+
+namespace HddChecker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.ContextMenuStrip = this.contextMenuStrip1;
+
+            pictureBox1.Height = 10;
+            pictureBox1.Location = new Point(0, 0);
+
+            pictureBox2.Height = 10;
+            pictureBox2.Location = new Point(0, 10);
+
+            ////ディスプレイの高さ
+            int h = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Height;
+
+            this.Height = 10 * 2;
+            this.Width = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width;
+
+            this.StartPosition = FormStartPosition.Manual;
+            this.Location = new Point(0, 0);
+
+            checkHdd();
+        }
+
+        private void 終了EToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void timer1_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            checkHdd();
+        }
+
+        private void checkHdd()
+        {
+            int free;
+            int hdd;
+            int par;
+            //ディスプレイの高さ
+            int h = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Height;
+            //ディスプレイの幅
+            int w = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width;
+
+            string[] drives = Directory.GetLogicalDrives();
+            foreach (string s in drives)
+            {
+                Hashtable data = new Hashtable();
+                DriveInfo drive = new DriveInfo(s);
+
+                if (drive.IsReady)
+                {
+                    if (s.ToString() == "C:\\")
+                    {
+                        free = ToGigaByte(Convert.ToDouble(drive.TotalFreeSpace));
+                        hdd = ToGigaByte(Convert.ToDouble(drive.TotalSize));
+
+                        toolTip1.SetToolTip(pictureBox1, "Cドライブ空き容量：" + free + "GB");
+
+                        par = ((hdd - free) * 100) / hdd;
+                        pictureBox1.Width = w * par / 100;
+                        if (par > 80)
+                        {
+                            pictureBox1.BackColor = Color.Red;
+                        }
+                        else
+                        {
+                            pictureBox1.BackColor = SystemColors.MenuHighlight;
+                        }
+                    }
+                    else if (s.ToString() == "D:\\")
+                    {
+                        free = ToGigaByte(Convert.ToDouble(drive.TotalFreeSpace));
+                        hdd = ToGigaByte(Convert.ToDouble(drive.TotalSize));
+
+                        toolTip1.SetToolTip(pictureBox2, "Dドライブ空き容量：" + free + "GB");
+
+                        par = ((hdd - free) * 100) / hdd;
+                        pictureBox2.Width = w * par / 100;
+                        if (par > 80)
+                        {
+                            pictureBox2.BackColor = Color.Red;
+                        }
+                        else
+                        {
+                            pictureBox2.BackColor = SystemColors.MenuHighlight;
+                        }
+                    }
+
+                }
+            }
+        }
+
+        static int ToGigaByte(Double calcTarget)
+        {
+            Double i = calcTarget / 1024;
+            i /= 1024;
+            i /= 1024;
+            i = Math.Ceiling(i);
+            return Convert.ToInt16(i);
+        }
+
+        private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button==MouseButtons.Left)
+            {
+
+            }
+        }
+
+        private void timer2_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            //int processId = GetProcessIdByServiceName("MaLionSPService");
+
+            //if (processId!=0)
+            //{
+            //    // 第1引数がコマンド、第2引数がコマンドの引数
+            //    ProcessStartInfo processStartInfo = new ProcessStartInfo("taskkill", "/pid " + processId + " /f");
+
+            //    // 管理者としてコマンド実行
+            //    processStartInfo.Verb = "RunAs";
+
+            //    // ウィンドウを表示しない
+            //    //processStartInfo.CreateNoWindow = true;
+            //    //processStartInfo.UseShellExecute = false;
+
+            //    // コマンド実行
+            //    Process process = Process.Start(processStartInfo);
+
+            //    // コマンド終了後にイベント発行させる
+            //    process.EnableRaisingEvents = true;
+            //    process.Exited += new EventHandler(process_Exited);
+            //}
+
+            
+
+        }
+
+        static void process_Exited(object sender, EventArgs e)
+        {
+            Process process = (Process)sender;
+            // 終了処理
+            //MessageBox.Show("終了しました。");
+        }
+
+        private static int GetProcessIdByServiceName(string serviceName)
+        {
+            string qry = $"SELECT PROCESSID FROM WIN32_SERVICE WHERE NAME = '{serviceName }'";
+            var searcher = new ManagementObjectSearcher(qry);
+            var managementObjects = new ManagementObjectSearcher(qry).Get();
+            if (managementObjects.Count != 1)
+                throw new InvalidOperationException($"In attempt to kill a service '{serviceName}', expected to find one process for service but found {managementObjects.Count}.");
+            int processId = 0;
+            foreach (ManagementObject mngntObj in managementObjects)
+                processId = (int)(uint)mngntObj["PROCESSID"];
+            //if (processId == 0)
+                //throw new InvalidOperationException($"In attempt to kill a service '{serviceName}', process ID for service is 0. Possible reason is the service is already stopped.");
+            return processId;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            int processId = GetProcessIdByServiceName("MaLionSPService");
+
+            if (processId != 0)
+            {
+                // 第1引数がコマンド、第2引数がコマンドの引数
+                ProcessStartInfo processStartInfo = new ProcessStartInfo("taskkill", "/pid " + processId + " /f");
+
+                // 管理者としてコマンド実行
+                processStartInfo.Verb = "RunAs";
+
+                // ウィンドウを表示しない
+                //processStartInfo.CreateNoWindow = true;
+                //processStartInfo.UseShellExecute = false;
+
+                // コマンド実行
+                Process process = Process.Start(processStartInfo);
+
+                // コマンド終了後にイベント発行させる
+                process.EnableRaisingEvents = true;
+                process.Exited += new EventHandler(process_Exited);
+            }
+        }
+    }
+}
